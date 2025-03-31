70dd5d12368cb5b61f7aa9975a24ff467a02b161
ilonagoergens <ilona.goergens@edu.techstarter.de>
2025-03-31 14:00:03 +0200

    fix: nginx.conf datei geändert

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://www.dreamsync.pro/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://3.124.116.208:3000/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
